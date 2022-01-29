98d133f03d041c5b98f619f068702f683ef673f6
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-01-29 22:26:09 +0000

    Tested useEffect in component to fetch data successfully

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Button() {
-  return <button type="button">Button</button>;
+  const [pokemon, setPokemon] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon/gengar");
+
+      let data = await response.json();
+
+      data = {
+        id: data.id,
+        name: data.name,
+        image: data.sprites.front_default,
+      };
+      console.log(data);
+    }
+
+    if (pokemon) {
+      fetchData();
+    }
+  }, [pokemon]);
+
+  function handleClick() {
+    setPokemon(true);
+  }
+
+  return (
+    <button type="button" onClick={handleClick}>
+      Button
+    </button>
+  );
 }
 
 export default Button;
